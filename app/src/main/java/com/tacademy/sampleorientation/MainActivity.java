a90d08043aeb4d4f1a41abe1c49b9edb93e6e218
River Pearl <riverpearl0115@gmail.com>
2016-08-02 15:14:26 +0900

    Initial commit

---
+++ app/src/main/java/com/tacademy/sampleorientation/MainActivity.java
@@ -0,0 +1,25 @@
+package com.tacademy.sampleorientation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn_orientation_activity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrientationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tacademy/sampleorientation/OrientationActivity.java
@@ -0,0 +1,13 @@
+package com.tacademy.sampleorientation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrientationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orientation);
+    }
+}
